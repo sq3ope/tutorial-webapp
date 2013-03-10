f2b79a7c2451470bb12d8c2935b781dea1f09961
Pawel Firlej <pawel.firlej@payu.pl>
2013-03-10 21:35:51 +0100

    Added jpa support
    Change-Id: I39461ad28381e702026a3971a39315bff111b53c

--- jsp-form/src/main/java/com/mycompany/app/spring/controller/HelloWorldController.java
+++ jsp-form/src/main/java/com/mycompany/app/spring/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.mycompany.app.spring.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,22 +14,27 @@ import com.mycompany.app.spring.model.HelloForm;
 @Controller
 public class HelloWorldController {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
 	@RequestMapping("/")
     public ModelAndView root(HelloForm form, BindingResult bindingResult, Model uiModel) {
 		return helloWorld(form, bindingResult, uiModel);
 	}
-	
+
 	@RequestMapping("/hello")
     public ModelAndView helloWorld(HelloForm form, BindingResult bindingResult, Model uiModel) {
+		entityManager.persist(form);
+
 		if (form.getFirstName() != null) {
 			form.setFirstName(form.getFirstName() + "x");
 		}
-		
+
 		if (form.getLastName() != null) {
 			form.setLastName(form.getLastName() + "x");
 		}
-		
+
         return new ModelAndView("hello", "form", form);
     }
-	
+
 }
--- jsp-form/src/main/java/com/mycompany/app/spring/model/HelloForm.java
+++ jsp-form/src/main/java/com/mycompany/app/spring/model/HelloForm.java
@@ -1,7 +1,26 @@
 package com.mycompany.app.spring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class HelloForm {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+	@Column
 	private String firstName;
+
+	@Column
+	private String lastName;
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -14,5 +33,4 @@ public class HelloForm {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	private String lastName;
 }
